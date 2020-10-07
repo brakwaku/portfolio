5ce9970a6efb8a0683bbad1c6b0497d2ceacfac7
BraKwaku Jr <brakwakujr@BraKwakus-MacBook-Pro.local>
2020-10-07 03:59:45 -0600

    Tried fixing resume anchor

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import initTilt from './js/tilt';
 import initSr from './js/sr';
-
 import './style/main.scss';
+import MyResume from './assets/resume.pdf';
+
+function addResume(MyResume) {
+  const elements = document.getElementsByClassName('.cta-btn--resume');
+  for(let el of elements){
+    el.href = url(MyResume);
+  }
+};
 
 $('a[href^="#"]').on('click', function(event) {
   var target = $(this.getAttribute('href'));
@@ -18,5 +25,7 @@ $('a[href^="#"]').on('click', function(event) {
   }
 });
 
+
+addResume();
 initSr();
 initTilt();
